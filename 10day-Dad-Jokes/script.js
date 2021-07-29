b60bbdea0908332b88f5fbc715cad388b046cff8
Oleg <devsniga@gmail.com>
2021-07-29 23:28:58 +0200

    Project 'Dad Jokes' is complete. Yu-Hu!

---
+++ 10day-Dad-Jokes/script.js
@@ -0,0 +1,38 @@
+const jokeEl = document.querySelector('#joke')
+const jokeBtn = document.querySelector('#jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke() {
+
+  const config = {
+    headers: {
+      'Accept' : 'application/json'
+    }
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
+
+}
+
+// function generateJoke() {
+
+//   const config = {
+//     headers: {
+//       'Accept' : 'application/json'
+//     }
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//   .then(response => response.json())
+//   .then(data => {
+//     jokeEl.innerHTML = data.joke
+//   })
+
+// }
